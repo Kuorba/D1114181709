f38f7b9fa1866f4605a1eb32d17ef63146a1984d
Kuroba <d1114181709@gm.lhu.edu.tw>
2024-10-04 15:46:37 +0800

    echo行列

--- table_gen.php
+++ table_gen.php
@@ -3,6 +3,19 @@
     $row_count = $_GET['row_num'];
     $col_count = $_GET['col_num'];
 
-    echo '表格大小 :' . '' . $row_count . '列 .' . $col_count . '列 .';
+    echo '表格大小 :' . '' . $row_count . '列 .' . $col_count . '行 .<br><br>';
+    
+    echo '<table border="1" style="border-collapse:collapse;">';
+    
+    for ($i = 0; $i < $row_count; $i++) {
+        echo '<tr>';
+        for ($j = 0; $j < $col_count; $j++) {
+            echo '<td>' . ($i + 1) . ',' . ($j + 1) . '</td>';
+        }
+        echo '</tr>';
+    }
+    
+    echo '</table>';
+?>
 
 ?>

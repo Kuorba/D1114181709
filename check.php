0dade9cd1cc7b7f08ef9802318a5539261dea266
Kuroba <d1114181709@gm.lhu.edu.tw>
2024-10-04 14:45:20 +0800

    php測試#2

--- check.php
+++ check.php
@@ -1,7 +1,7 @@
 <?php
 
-    $account = $_GET['acc'];
-    $password = $_GET['pass'];
+    $account = $_GET['account'];
+    $password = $_GET['password'];
     $check_code = $_GET['code'];
 
     echo '帳號 :' . $account .',密碼 :' . $password . '驗證碼 :' . $check_code;

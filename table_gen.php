6b80753730ab7c40213acb6d205c367d9e9ef2cf
Kuroba <d1114181709@gm.lhu.edu.tw>
2024-10-08 12:00:46 +0800

    新增選項表單 留言

--- table_gen.php
+++ table_gen.php
@@ -2,10 +2,16 @@
 
     $row_count = $_GET['row_num'];
     $col_count = $_GET['col_num'];
+    $gender = $_GET["gender"];
+    $dept = $_GET["dept"];
+    $msg = $_GET["message"];
 
-    echo '表格大小 :' . '' . $row_count . '列 .' . $col_count . '行 .<br><br>';
+    echo '表格大小 :' . $row_count . '列，' . $col_count . '行，';
+    echo '性別'. $gender . '，';
+    echo '科系'. $dept . '，';
+    echo '留言'. $msg . '，';
     
-    echo '<table border="1" style="border-collapse:collapse;">';
+    echo '<table border=\"1\">';
     
     for ($i = 0; $i < $row_count; $i++) {
         echo '<tr>';
